d61e982adeafdc7b39cf643ded51bbc0e96d36a8
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2024-08-22 12:40:07 -0500

    Hice la clase Todo con sus respectivos atributos

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,11 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str,) -> None:
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed = False
+        self.tags = []
+
+
+     
+
