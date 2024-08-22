a1d12519590944427b62a42fa28ebdb36495f435
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2024-08-22 13:06:19 -0500

    defini el metodo __str__ retornando el string

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -13,6 +13,11 @@ class Todo:
 
         if tag not in self.tags:
             self.tags.append(tag)
+    
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+        
+
 
 
 
