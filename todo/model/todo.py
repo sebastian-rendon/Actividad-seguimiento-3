0d7f5b5141608664ff54dc6181ed02ef2c28590e
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2024-08-22 12:55:04 -0500

    Agregue el metodo mark_completed y add_tag 2,3

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,10 +1,20 @@
 class Todo:
-    def __init__(self, code_id: int, title: str, description: str,) -> None:
+    def __init__(self, code_id: int, title: str, description: str) -> None:
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed = False
         self.tags = []
+
+    def mark_completed(self):
+        self.completed = True
+    
+    def add_tag(self, tag: str):
+
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+
 
 
      
