5ed838a462889d0ae80262bfc9a4b5fc0d211a49
juan sebastian <srendon521@gmail.com>
2024-08-27 12:44:56 -0500

    completo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -23,7 +23,31 @@ class TodoBook:
         self.todos: dict[int, Todo] = {}
 
     def add_todo(self, title: str, description: str) -> int:
-        id = len(self.todos) + 1
+        todo_id = len(self.todos) + 1
+        todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = todo
+        return todo_id
+
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if todo.completed]
+
+    def tags_todo_count(self) -> dict[str, int]:
+        tags_count: dict[str, int] = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag in tags_count:
+                    tags_count[tag] += 1
+                else:
+                    tags_count[tag] = 1
+        return tags_count
+
+
+        
+
+
 
 
 
